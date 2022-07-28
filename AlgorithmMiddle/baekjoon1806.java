5767874383cf80dc86d4ca562150a1e507706e02
jgb1123 <rmsqja2@gmail.com>
2022-07-28 22:10:09 +0900

    문제 풀이

--- AlgorithmMiddle/baekjoon1806.java
+++ AlgorithmMiddle/baekjoon1806.java
@@ -20,31 +20,32 @@ public class baekjoon1806 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int S = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N+1];
-        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());   // 수열의 길이
+        int S = Integer.parseInt(st.nextToken());   // 부분합의 비교 기준이 되는 값
+
+        int[] arr = new int[N+1];   // 수열 배열로 생성
+       st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int L = 0;
-        int R = 0;
-        int sum = 0;
-        int length;
-        int min = Integer.MAX_VALUE;
-        while(R<=N) {
-            if(sum>=S){
-                sum -= arr[L];
-                L++;
-                length = R - L + 1;
-                if(length<min) min = length;
-            }else{
-                sum += arr[R];
-                R++;
+        int L = 0;  // 왼쪽 포인터
+        int R = 0;  // 오른쪽 포인터
+        int sum = 0;    // 합
+        int length; // 길이
+        int min = Integer.MAX_VALUE;    // 최소 길이 int의 max값으로 초기화
+        while(R<=N) {   // 오른쪽 포인터가 N보다 작을 때 까지만 반복
+            if(sum>=S){ // 만약 S보다 sum이 크면
+                sum -= arr[L];  // sum에서 arr[L]을 빼주고
+                L++;    // L++
+                length = R - L + 1; // 길이는 R-L에 +1을 해줘야 한다. (사실 처음 출력보고 +1 놓쳤던거 알아챔)
+                if(length<min) min = length;    // 길이가 최소길이보다 작으면 갱신
+            }else{  // 만약 S보다 sum이 작으면
+                sum += arr[R];  // sum에 arr[R]을 더해준다
+                R++;    // R++
             }
         }
-        if(min != Integer.MAX_VALUE) System.out.println(min);
-        else System.out.println(0);
+        if(min != Integer.MAX_VALUE) System.out.println(min);   // 최소길이 min이 처음 초기화한 값이 아니면 min출력
+        else System.out.println(0); // 처음 초기화한 값 그대로면 0 출력
     }
 }

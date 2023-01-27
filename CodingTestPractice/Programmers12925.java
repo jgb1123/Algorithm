d7049ae6aa022bcee32c60f46c932a427ee5103d
jgb1123 <rmsqja2@gmail.com>
2023-01-27 22:25:53 +0900

    문제 풀이

---
+++ CodingTestPractice/Programmers12925.java
@@ -0,0 +1,15 @@
+// 문자열을 정수로 바꾸기(https://school.programmers.co.kr/learn/courses/30/lessons/12925)
+
+class Programmers12925 {
+    public int solution(String s) {
+        int answer;
+        if(s.charAt(0) == '-') {    // 맨 앞 문자가 '-'면
+            answer = -Integer.parseInt(s.substring(1, s.length())); // 맨 앞 글자를 제외한 문자열을 Integer로 변환 후 -로
+        } else if(s.charAt(0) == '+') { // 맨 앞 문자가 '+'면
+            answer = Integer.parseInt(s.substring(1, s.length()));  // 맨 앞 글자를 제외한 문자열을 Integer로 변환
+        } else {
+            answer = Integer.parseInt(s);   // 그 외에는 그냥 Integer로 변환
+        }
+        return answer;
+    }
+}
---
+++ CodingTestPractice/Programmers12935.java
@@ -0,0 +1,19 @@
+// 제일 작은 수 제거하기(https://school.programmers.co.kr/learn/courses/30/lessons/12935)
+
+import java.util.*;
+
+class Programmers12935 {
+    public int[] solution(int[] arr) {
+        if(arr.length == 1) return new int[]{-1};   // 배열의 길이가 1이면 {-1} 배열 리턴
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < arr.length; i++) {   // 가장 작은 수를 찾음
+            if(arr[i] < min) min = arr[i];
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] != min) list.add(arr[i]); // 가장 작은수가 아니면 list에 저장함
+        }
+        int[] answer = list.stream().mapToInt(i -> i).toArray();    // list를 int[]로 변환
+        return answer;  // 최종 반환
+    }
+}
